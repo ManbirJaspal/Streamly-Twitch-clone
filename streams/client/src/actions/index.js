ed656c1e769660b3ba9cfc5a226bbb0c4b693d85
Manbir Jaspal <manb.jaspal@gmail.com>
2019-05-21 16:56:36 -0400

    changes

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -21,7 +21,7 @@ export const signOut = () => {
   };
 };
 
-export const  = formValues => async (dispatch, getState) => {
+export const createStream = formValues => async (dispatch, getState) => {
   const { userId } = getState().auth;
   const response = await streams.post('/streams', {...formValues, userId });
   dispatch ({
