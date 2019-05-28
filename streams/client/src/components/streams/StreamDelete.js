2f9d46fb4c1619cf3904b316786aafb4badb2d8b
Manbir Jaspal <manb.jaspal@gmail.com>
2019-05-28 11:05:10 -0400

    React portals created

--- streams/client/src/components/streams/StreamDelete.js
+++ streams/client/src/components/streams/StreamDelete.js
@@ -1,26 +1,52 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import { Link } from 'react-router-dom';
 import Modal from '../Modal';
+import history from '../../history';
+import { fetchStream } from '../../actions';
+import { deleteStream } from '../../actions';
 
-const StreamDelete = () => {
+class StreamDelete extends React.Component {
 
-  const actions = (
-    <React.Fragment>
-      <button className="ui button negative">Delete</button>
-      <button className="ui button">Cancel</button>
+componentDidMount() {
+  this.props.fetchStream(this.props.match.params.id);
+}
 
-    </React.Fragment>
-  )
+renderActions() {
+  const { id } = this.props.match.params;
   return (
-    <div>
-      StreamDelete
-      <Modal
-        title="Delete Stream"
-        content="Are you sure you want to delete this stream ?"
-        actions={actions}
-        />
-    </div>
+    <React.Fragment>
+      <button onClick={() => this.props.deleteStream(id)} className="ui button negative">Delete</button>
+      <Link to="/" className="ui button">Cancel</Link>
+    </React.Fragment>
+  );
+}
+
+renderContent() {
+  if (!this.props.stream) {
+    return 'Are you sure you want to delete this stream?'
+  }
+    return `Are you sure you want to delete this stream with title: ${this.props.stream.title}`
+}
+
+
+
+  render() {
+    return (
+        <Modal
+          title="Delete Stream"
+          content={this.renderContent()}
+          actions={this.renderActions()}
+          onDismiss={() => history.push('/')}
+          />
     );
+  }
+}
 
-};
+const mapStateToProps = (state, ownProps) => {
+  return {
+    stream: state.streams[ownProps.match.params.id]
+  }
+}
 
-export default StreamDelete;
+export default connect(mapStateToProps, { fetchStream, deleteStream })(StreamDelete);
